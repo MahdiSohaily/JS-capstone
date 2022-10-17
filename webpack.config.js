706594f4fdbdccba6c7a62be6875d56d78741415
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-10-17 20:27:15 +0430

    Update webpack.config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,39 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
- module.exports = {
-   entry: {
-     index: './src/index.js',
-     print: './src/print.js',
-   },
+module.exports = {
+  entry: {
+    index: './src/index.js',
+  },
+  devtool: 'inline-source-map',
+  mode: 'development',
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      template: './src/index.html',
     }),
   ],
-   output: {
-     filename: '[name].bundle.js',
-     path: path.resolve(__dirname, 'dist'),
-   },
- };
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+    ],
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'images/[name][ext]',
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};

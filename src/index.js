73291bd4f68b2fff22733f925384067c5c22d4e1
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-10-18 23:35:30 +0430

    Add pagination active state

--- src/index.js
+++ src/index.js
@@ -6,6 +6,16 @@ import getProducts from './Modules/GetProducts.js';
 
 let allProducts = [];
 
+const addPagination = () => {
+  const pagination = document.querySelectorAll('.page-item');
+  pagination.forEach((item) => {
+    item.addEventListener('click', (e) => {
+      // eslint-disable-next-line no-use-before-define
+      renderTemplate(Number(e.target.getAttribute('data-index')));
+    });
+  });
+};
+
 const renderTemplate = (index = 0) => {
   const start = index * 6;
   const container = document.querySelector('#app');
@@ -21,14 +31,15 @@ const renderTemplate = (index = 0) => {
   </div>
   <ul class='pagination'>
     <li><a class='page-item' data-index='0'>&laquo;</a></li>
-    <li><a class='page-item active' data-index='0'>1</a></li>
-    <li><a class='page-item' data-index='1'>2</a></li>
-    <li><a class='page-item' data-index='2'>3</a></li>
-    <li><a class='page-item' data-index='3'>4</a></li>
+    <li><a class='page-item ${index === 0 ? 'active' : ''}' data-index='0'>1</a></li>
+    <li><a class='page-item ${index === 1 ? 'active' : ''}' data-index='1'>2</a></li>
+    <li><a class='page-item ${index === 2 ? 'active' : ''}' data-index='2'>3</a></li>
+    <li><a class='page-item ${index === 3 ? 'active' : ''}' data-index='3'>4</a></li>
     <li><a class='page-item' data-index='3'>&raquo;</a></li>
   <ul>
   `;
   container.innerHTML = template;
+  addPagination();
 };
 
 window.addEventListener('load', () => {

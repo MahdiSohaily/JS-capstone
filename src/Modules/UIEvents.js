5981a33faf76d9122e508739384ae16ee426f9f2
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-10-18 20:13:20 +0430

    Add window resize event to display

--- src/Modules/UIEvents.js
+++ src/Modules/UIEvents.js
@@ -10,5 +10,18 @@ menuIcon.addEventListener('click', (e) => {
 });
 
 closeMenu.addEventListener('click', () => {
-  navList.classList.remove('open');
+  menuIcon.style.display = 'block';
+  navList.style.display = 'none';
+});
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth > 800) {
+    menuIcon.style.display = 'none';
+    navList.classList.remove('open');
+    navList.style.display = 'block';
+  } else {
+    navList.classList.add('open');
+    menuIcon.style.display = 'block';
+    navList.style.display = 'none';
+  }
 });

798802eec58f0d909f36f4e6970de17258e3cb0c
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-10-19 16:46:56 +0430

    Add comments to the functions to be more descriptive

--- src/Modules/GetProducts.js
+++ src/Modules/GetProducts.js
@@ -1,4 +1,9 @@
-/* eslint-disable linebreak-style */
+/**
+ * This function send a GET request
+ * to an external API to get some dummy
+ * data about products od an store and
+ * @returns a Promise contains the data
+ */
 const getProducts = async () => {
   const response = await fetch('https://fakestoreapi.com/products');
   const data = await response.json();

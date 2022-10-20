e55c3c27cfed58f46c43bde292acfb13b76026a3
AlejandraKeber <alejandrakeber@gmail.com>
2022-10-20 11:39:23 -0500

    Update displayComments file

--- src/Modules/displaycomments.js
+++ src/Modules/displaycomments.js
@@ -1,11 +1,12 @@
 const showComments = (comments) => {
   let template = '';
-  comments.forEach((element, index) => {
-    template += `<li>${index + 1} - ${element.username} - ${
-      element.comment
-    } - ${element.creation_date}</li>`;
-  });
-
+  if (Array.isArray(comments)) {
+    comments.forEach((element, index) => {
+      template += `<li>${index + 1} - ${element.username} - ${
+        element.comment
+      } - ${element.creation_date}</li>`;
+    });
+  }
   return template;
 };
 const appendComment = () => {};

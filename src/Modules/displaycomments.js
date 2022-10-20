f6aafac996c4c63073cd46f13a423f4fd169affd
AlejandraKeber <alejandrakeber@gmail.com>
2022-10-20 11:07:51 -0500

    Update displayComments file

--- src/Modules/displaycomments.js
+++ src/Modules/displaycomments.js
@@ -1,33 +1,10 @@
-import Comments from './Comments.js';
+const showComments = (comments) => {
+  let template = '';
+  comments.forEach((element) => {
+    template += `<li>${element.creation_date}</li>`;
+  });
 
-const showComments = async () => {
-  const com = await Comments.getComments();
-  const resultsBox = document.querySelector('.comments-container');
-  const fragment = new DocumentFragment();
-  resultsBox.innerHTML = '';
-  if (com.length === 0) {
-    const paragraph = document.createElement('p');
-    paragraph.innerText = 'No comments';
-    resultsBox.append(paragraph);
-  } else {
-    com.forEach((element) => {
-      const { username, comment } = element;
-      const paragraph = document.createElement('p');
-      paragraph.classList.add('comment-list');
-      paragraph.innerText = `${username}: ${comment}`;
-      fragment.append(paragraph);
-    });
-    resultsBox.append(fragment);
-  }
+  return template;
 };
-
-const addComment = async () => {
-  const inputs = document.querySelector('form').elements;
-  const [username, comment] = [inputs[0].value, inputs[1].value];
-  await Comments.setComments(username, comment);
-  inputs[0].value = '';
-  inputs[1].value = '';
-  showComments();
-};
-
-export { showComments, addComment };
+const appendComment = () => {};
+export { showComments, appendComment };

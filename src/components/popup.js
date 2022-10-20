790469e39f7443a0a0e48e95690e1006aa4323a0
AlejandraKeber <alejandrakeber@gmail.com>
2022-10-20 11:46:14 -0500

    Change popup file

--- src/components/popup.js
+++ src/components/popup.js
@@ -1,4 +1,4 @@
-const showPopup = (product, comments) => `<div class="popup-container">
+const showPopup = (product, comments, count) => `<div class="popup-container">
 <div class="popup-info">
 <img width="20" height="20" class="popup-close" src="./images/icon-close.svg" alt="close icon">
 <div class="prod-detail">
@@ -8,7 +8,9 @@ const showPopup = (product, comments) => `<div class="popup-container">
   <div class="pt-500">
     <div class="description">
       <h4 class="text-orange">Clara Store</h4>
-      <h3 class="secondary-heading text-very-dark-blue mb-200">${product.title}</h3>
+      <h3 class="secondary-heading text-very-dark-blue mb-200">${
+  product.title
+}</h3>
       <p class="text-grayish-blue prod-desc mb-300">
         ${product.description}.
       </p>
@@ -18,7 +20,7 @@ const showPopup = (product, comments) => `<div class="popup-container">
 </div>
 <div class="prod-comments">
   <div class="comment-list">
-    <h3 class='comments-counters '>Comments (<span class="counter">0</span>) </h3>
+    <h3 class='comments-counters '>Comments (<span class="counter">${Number.isNaN(count) ? count : 0}</span>) </h3>
     <ul class="comments-container border mt-100">
       ${comments}
     </ul>
@@ -31,7 +33,9 @@ const showPopup = (product, comments) => `<div class="popup-container">
         <input type="text" name="name" aria-label="user name" class="name p-200" placeholder="Your name" required>
           <textarea name="description" aria-label="user message" class="message p-200" cols="30" rows="5"
             placeholder="Your insight..." required></textarea>
-          <button id=${product.id} type="submit" class="submit input p-200">Comment</button>
+          <button id=${
+  product.id
+} type="submit" class="submit input p-200">Comment</button>
     </form>
   </div>
 </div>

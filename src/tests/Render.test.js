ecd4f77f51f984461df6b529651f7a9cfb9acc5e
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-10-21 15:28:34 +0430

    Update the test function

--- src/tests/Render.test.js
+++ src/tests/Render.test.js
@@ -6,8 +6,7 @@ import { countLikes } from '../Modules/Render.js';
 describe('Addition test', () => {
   // Test One
   test('Adding TODO One to local storage', () => {
-    const allLikedItems = [{ likes: 5, item_id: '1' }];
-    console.log(countLikes(allLikedItems, 1));
-    expect(1).toEqual(1);
+    const allLikedItems = [{ likes: 5, item_id: 1 }];
+    expect(countLikes(allLikedItems, 1)).toBe(5);
   });
 });

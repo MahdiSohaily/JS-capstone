2df0bf8197ebe95c78cbd49eef4594df50007e72
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-10-21 15:38:30 +0430

    Added more test cases

--- src/tests/Render.test.js
+++ src/tests/Render.test.js
@@ -3,10 +3,22 @@
  */
 import { countLikes } from '../Modules/Render.js';
 
-describe('Addition test', () => {
+describe('Counting the number of likes for specific item test', () => {
   // Test One
   test('Adding TODO One to local storage', () => {
     const allLikedItems = [{ likes: 5, item_id: 1 }];
     expect(countLikes(allLikedItems, 1)).toBe(5);
+  });
+
+  // Test Two
+  test('Adding TODO One to local storage', () => {
+    const allLikedItems = [{ likes: 10, item_id: 20 }];
+    expect(countLikes(allLikedItems, 20)).toBe(10);
+  });
+
+  // Test Three
+  test('Adding TODO One to local storage', () => {
+    const allLikedItems = [{ likes: 1000, item_id: 18 }];
+    expect(countLikes(allLikedItems, 18)).toBe(1000);
   });
 });
